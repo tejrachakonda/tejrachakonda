84531726d9d880b5b1da7a40b64e496ca4209d80
tejrachakonda <itsstej@gmail.com>
2024-08-13 19:13:25 +0530

    Delete Firstprogram.java

--- Firstprogram.java
+++
@@ -1,8 +0,0 @@
-import java.io.*;
-class Firstprogram
-{    //Beginning of class
-       public static void main(String args[])
-       { // begining of main method
-              System.out.println("welcome to java");
-       } // end of main method
-}    // end of class
